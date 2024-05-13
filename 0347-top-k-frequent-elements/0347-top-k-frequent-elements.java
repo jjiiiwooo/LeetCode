e47b002f1319afaa23ba1394182f0412f72b56c2
jjiiiwooo <150234478+jjiiiwooo@users.noreply.github.com>
2024-05-14 00:27:28 +0900

    Time: 13 ms (76.18%), Space: 48.8 MB (25.45%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -9,35 +9,20 @@ class Solution {
             frequencyMap.put(n,frequencyMap.getOrDefault(n,0)+1);
         }
 
-        //빈도수 기준으로 각 엘리먼트를 저장할 해시맵 선언, 빈도수=>엘리먼트
-        Map<Integer, List<Integer>>buckets = new HashMap<>();
-        //각 엘리먼트를 반복하며 이번에는 빈도수를 키로, 엘리먼트를 값으로 저장 
-        for(int elem : frequencyMap.keySet()) {
-            int frequency = frequencyMap.get(elem);
-            //빈도수에 해당하는 엘리먼트가 존재하지 않으면 빈 리스트 생성
-            List<Integer>elems = buckets.getOrDefault(frequency, new ArrayList<>());
-            //저장할 값에 엘리먼트 추가
-            elems.add(elem);
-            //빈도수를 키로, 엘리먼트를 값으로 저장
-            buckets.put(frequency, elems);
+        //빈도순으로 정렬되는 우선순위 큐 선언
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> b[1]-a[1]);
+        //우선순위 큐에 각 엘리먼트와 빈도수 삽입
+        for(int elem : frequencyMap.keySet())
+        {
+            pq.add(new int[]{elem, frequencyMap.get(elem)});
         }
 
-        //결과로 리턴할 변수 선언, k번까지만 추출하면 되므로 k만큼 선언
+        //결과로 리턴할 변수 선언
         int result[] = new int[k];
-        int idx = 0;
-        //전체 엘리먼트 수에서 하나씩 내려가며 해당하는 빈도의 엘리먼트 추출
-        for(int fq = nums.length; fq>=0&& idx<k; fq--)
+        //k번까지만 반복하여 우선순위 큐에서 결과 추출
+        for(int i=0; i<k; i++)
         {
-            //해당 빈도에 값이 존재한다면 엘리먼트를 결과 변수에 삽입
-            if(buckets.get(fq)!=null)
-            {
-                //해당 빈도에 여러 엘리먼트가 있을 수 있으므로 반복하여 모두 삽입
-                for(int elem : buckets.get(fq))
-                {
-                    result[idx] = elem;
-                    idx++;
-                }
-            }
+            result[i] = pq.poll()[0];
         }
         return result;
     }
